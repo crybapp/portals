89b7ff0082957a9a223c3fc45b27db5126823737
Anthony Anaya <41459508+Flexa-B@users.noreply.github.com>
2019-10-15 22:16:49 -0400

    Added Config Stuff To Config File

--- src/config/providers/digitalocean.config.ts
+++ src/config/providers/digitalocean.config.ts
@@ -1,1 +1,6 @@
-export default test
+import { DigitalOcean } from 'dots-wrapper';
+
+const myApiToken = 'c5380bb188bd8dace0c1e76fad3213e8d6e9dae973b3220874e847d1036e2c9e';
+const digitalOcean = new DigitalOcean(myApiToken);
+
+export default digitalOcean
