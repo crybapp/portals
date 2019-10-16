4005fe5429089506f504ba2aad814d0746f21ee3
Anthony Anaya <41459508+Flexa-B@users.noreply.github.com>
2019-10-15 22:09:14 -0400

    Created First Drafts Of Droplet Management

--- src/drivers/digitalocean.driver.ts
+++ src/drivers/digitalocean.driver.ts
@@ -7,18 +7,52 @@ const myApiToken = 'c5380bb188bd8dace0c1e76fad3213e8d6e9dae973b3220874e847d1036e
 const digitalOcean = new DigitalOcean(myApiToken);
 
 export function testFunction() {
-    digitalOcean.Account.get().subscribe(
+    /* digitalOcean.Account.get().subscribe(
         account => console.log(account),
         err => console.log(err.message),
         () => console.log("complete")
-    )
+    ) */
+
+/*     let dropletSpecs = {
+        name:"TestDropletFromCode1",
+        region:"nyc3",
+        size:"s-1vcpu-1gb",
+        image:"ubuntu-16-04-x64",
+        backups:false,
+        ipv6:true,
+        tags: ["stream"]
+    }
+
+    digitalOcean.Droplet.create(dropletSpecs).subscribe(
+        dropletInfo => console.log(dropletInfo),
+        err => console.log(err.message),
+        () => console.log("complete")
+    ) */
 }
 
 export const openPortalInstance = async (portal: Portal) => {
     const name = `portal-${portal.id}`
 
     try {
-        // Create the server using the API & Provider of your choice
+        let dropletSpecs = {
+            name:name,
+            region:"nyc3",
+            size:"s-1vcpu-1gb",
+            image:"ubuntu-16-04-x64",
+            backups:false,
+            ipv6:true,
+            tags: ["stream"]
+        }
+
+        digitalOcean.Droplet.create(dropletSpecs).subscribe(
+            dropletInfo => function() {
+                console.log(dropletInfo)
+                portal.serverId = dropletInfo.id.toString()
+            },
+            err => console.log(err.message),
+            () => console.log("CreateDropletComplete")
+        )
+
         await portal.updateStatus('starting')
 
         console.log(`opened portal with name ${name}`)
@@ -33,7 +67,11 @@ export const closePortalInstance = async (portal: Portal) => {
     const name = `portal-${portal.id}`, { serverId } = portal
 
     try {
-        // Destroy the server using the id of the server you stored when creating the server
+        digitalOcean.Droplet.delete(+serverId).subscribe(
+            dropletInfo => console.log(dropletInfo),
+            err => console.log(err.message),
+            () => console.log("DeleteDropletComplete")
+        )
 
         console.log(`closed portal with name ${name}`)
     } catch(error) {
