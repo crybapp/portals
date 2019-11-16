4ee5b40612b1d567d8db7d9689422a5ca6f75315
‮ymmaj <69@users.noreply.github.com>
2019-11-16 11:57:20 +0100

    Add Hetzner Cloud driver (#53)
    * begin working on hetzner cloud support
    
    * rewrite import
    
    * throw an error if unable to find portal
    
    * remove leftover from google cloud ctrlc ctrlv
    
    * finalize things
    
    * add hetzner cloud to router
    
    * add hcloud-js to dependencies, change image name to id for deploy
    
    * move imageId to variable and add guide to creating an image
    
    * probably a better idea to move it to .env
    
    * move Hetzner Cloud image ID to .env
    
    * fix indentation
    
    * remove semicolons
    
    * make zoneId and serverType customizable via env
    
    * Update lockfile

---
+++ src/config/providers/hetznercloud.config.ts
@@ -0,0 +1,15 @@
+import { Client } from 'hcloud-js'
+
+export const fetchCredentials = () => {
+    let credentials: any = process.env.HETZNER_API_KEY
+    if(!credentials) return null
+
+    return credentials
+}
+
+export const createClient = () => {
+    const credentials = fetchCredentials()
+    if(!credentials) return null
+
+    return new Client(credentials)
+}
