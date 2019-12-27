f66c8de3f8f96747b21c3d223ef60d705fa134c8
William Gibson <w.gibbo3@icloud.com>
2019-12-27 23:37:06 +0000

    Update service port from 1337 to 5000

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import server from './server'
 
-const port = process.env.PORT || 1337
+const port = process.env.PORT || 5000
 server.listen(port, () => console.log(require('fs').readFileSync('logo.txt', 'utf8').replace(':PORT', `:${port}`)))
